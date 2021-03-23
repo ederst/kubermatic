ederst/kubermatic

commit 6f986274f668ef84a04f371a1767ea3b4e0fe9ff

[release/v2.16] Add support for Hetzner CCM (#6588) (#6619)

pkg/resources/cloudcontroller/openstack.go
@@ -18,9 +18,7 @@ package cloudcontroller
 
 import (
 	"fmt"
-	"net/url"
 
-	kubermaticv1 "k8c.io/kubermatic/v2/pkg/crd/kubermatic/v1"
 	"k8c.io/kubermatic/v2/pkg/resources"
 	"k8c.io/kubermatic/v2/pkg/resources/reconciling"
 	"k8c.io/kubermatic/v2/pkg/resources/vpnsidecar"
@@ -61,8 +59,6 @@ func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedD
 				MatchLabels: resources.BaseAppLabels(osName, nil),
 			}
 
-			dep.Spec.Template.Spec.Volumes = getOSVolumes()
-
 			podLabels, err := data.GetPodTemplateLabels(osName, dep.Spec.Template.Spec.Volumes, nil)
 			if err != nil {
 				return nil, err
@@ -81,40 +77,42 @@ func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedD
 			f := false
 			dep.Spec.Template.Spec.AutomountServiceAccountToken = &f
 
-			openvpnSidecar, err := vpnsidecar.OpenVPNSidecarContainer(data, "openvpn-client")
+			openvpnSidecar, err := vpnsidecar.OpenVPNSidecarContainer(data, openvpnClientContainerName)
 			if err != nil {
 				return nil, fmt.Errorf("failed to get openvpn sidecar: %v", err)
-			}
-
-			osCloudProviderMounts := []corev1.VolumeMount{
-				{
-					Name:      resources.CloudControllerManagerKubeconfigSecretName,
-					MountPath: "/etc/kubernetes/kubeconfig",
-					ReadOnly:  true,
-				},
-				{
-					Name:      resources.CloudConfigConfigMapName,
-					MountPath: "/etc/kubernetes/cloud",
-					ReadOnly:  true,
-				},
 			}
 
 			version, err := getOSVersion(data.Cluster().Spec.Version)
 			if err != nil {
 				return nil, err
 			}
-			flags := getOSFlags(data)
+
+			dep.Spec.Template.Spec.Volumes = append(getVolumes(), corev1.Volume{
+				Name: resources.CloudConfigConfigMapName,
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: resources.CloudConfigConfigMapName,
+						},
+					},
+				},
+			})
 
 			dep.Spec.Template.Spec.Containers = []corev1.Container{
 				*openvpnSidecar,
 				{
-					Name:         osName,
-					Image:        data.ImageRegistry(resources.RegistryDocker) + "/k8scloudprovider/openstack-cloud-controller-manager:v" + version,
-					Command:      []string{"/bin/openstack-cloud-controller-manager"},
-					Args:         flags,
-					VolumeMounts: osCloudProviderMounts,
+					Name:    osName,
+					Image:   data.ImageRegistry(resources.RegistryDocker) + "/k8scloudprovider/openstack-cloud-controller-manager:v" + version,
+					Command: []string{"/bin/openstack-cloud-controller-manager"},
+					Args:    getOSFlags(data),
+					VolumeMounts: append(getVolumeMounts(), corev1.VolumeMount{
+						Name:      resources.CloudConfigConfigMapName,
+						MountPath: "/etc/kubernetes/cloud",
+						ReadOnly:  true,
+					}),
 				},
 			}
+
 			defResourceRequirements := map[string]*corev1.ResourceRequirements{
 				osName:              osResourceRequirements.DeepCopy(),
 				openvpnSidecar.Name: openvpnSidecar.Resources.DeepCopy(),
@@ -126,37 +124,6 @@ func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedD
 
 			return dep, nil
 		}
-	}
-}
-
-func getOSVolumes() []corev1.Volume {
-	return []corev1.Volume{
-		{
-			Name: resources.CloudConfigConfigMapName,
-			VolumeSource: corev1.VolumeSource{
-				ConfigMap: &corev1.ConfigMapVolumeSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: resources.CloudConfigConfigMapName,
-					},
-				},
-			},
-		},
-		{
-			Name: resources.OpenVPNClientCertificatesSecretName,
-			VolumeSource: corev1.VolumeSource{
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: resources.OpenVPNClientCertificatesSecretName,
-				},
-			},
-		},
-		{
-			Name: resources.CloudControllerManagerKubeconfigSecretName,
-			VolumeSource: corev1.VolumeSource{
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: resources.CloudControllerManagerKubeconfigSecretName,
-				},
-			},
-		},
 	}
 }
 
@@ -185,39 +152,6 @@ func getOSVersion(version semver.Semver) (string, error) {
 	default:
 		return "1.19.2", nil
 	}
-}
-
-// ExternalCloudControllerFeatureSupported checks if the
-func ExternalCloudControllerFeatureSupported(dc *kubermaticv1.Datacenter, cluster *kubermaticv1.Cluster) bool {
-	if cluster.Spec.Cloud.Openstack == nil {
-		return false
-	}
-	// When using OpenStack external CCM with Open Telekom Cloud the creation
-	// of LBs fail as documented in the issue below:
-	// https://github.com/kubernetes/cloud-provider-openstack/issues/960
-	// Falling back to the in-tree CloudProvider mitigates the problem, even if
-	// not all features are expected to work properly (e.g.
-	// `manage-security-groups` should be set to false in cloud config, see
-	// https://kubernetes.io/docs/concepts/cluster-administration/cloud-providers/#load-balancer
-	// for more details).
-	//
-	// TODO(irozzo) This is a dirty hack to temporarily support OTC using
-	// Openstack provider, remove this when dedicated OTC support is
-	// introduced in Kubermatic.
-	if dc.Spec.Openstack != nil && isOTC(dc.Spec.Openstack) {
-		return false
-	}
-	return OpenStackCloudControllerSupported(cluster.Spec.Version)
-}
-
-// isOTC returns `true` if the OpenStack Datacenter uses OTC (i.e.
-// Open Telekom Cloud), `false` otherwise.
-func isOTC(dc *kubermaticv1.DatacenterSpecOpenstack) bool {
-	u, err := url.Parse(dc.AuthURL)
-	if err != nil {
-		return false
-	}
-	return u.Host == "iam.eu-de.otc.t-systems.com"
 }
 
 // OpenStackCloudControllerSupported checks if this version of Kubernetes is supported

cmd/conformance-tests/custom_tests.go
@@ -164,6 +164,9 @@ func (r *testRunner) testLB(ctx context.Context, log *zap.SugaredLogger, userClu
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "test",
 			Namespace: ns.Name,
+			Annotations: map[string]string{
+				"load-balancer.hetzner.cloud/location": "nbg1",
+			},
 		},
 		Spec: corev1.ServiceSpec{
 			Type:     corev1.ServiceTypeLoadBalancer,

cmd/conformance-tests/runner.go
@@ -1368,7 +1368,8 @@ func supportsStorage(cluster *kubermaticv1.Cluster) bool {
 func supportsLBs(cluster *kubermaticv1.Cluster) bool {
 	return cluster.Spec.Cloud.Azure != nil ||
 		cluster.Spec.Cloud.AWS != nil ||
-		cluster.Spec.Cloud.GCP != nil
+		cluster.Spec.Cloud.GCP != nil ||
+		(cluster.Spec.Cloud.Hetzner != nil && cluster.Spec.Version.Minor() >= 18)
 }
 
 func (r *testRunner) printAllControlPlaneLogs(ctx context.Context, log *zap.SugaredLogger, clusterName string) {

docs/zz_generated.seed.yaml
@@ -97,6 +97,10 @@ spec:
           # Optional: Detailed location of the datacenter, like "Hamburg" or "Datacenter 7".
           # For informational purposes only.
           location: ""
+          # Network is the pre-existing Hetzner network in which the machines are running.
+          # While machines can be in multiple networks, a single one must be chosen for the
+          # HCloud CCM to work.
+          network: ""
         kubevirt:
           # DNSConfig represents the DNS parameters of a pod. Parameters specified here will be merged to the generated DNS
           # configuration based on DNSPolicy.

hack/ci/testdata/seed.yaml
@@ -40,7 +40,7 @@ spec:
       location: Frankfurt
       country: DE
       spec:
-         bringyourown: {}
+        bringyourown: {}
     alibaba-eu-central-1a:
       location: Frankfurt
       country: DE
@@ -59,6 +59,7 @@ spec:
       spec:
         hetzner:
           datacenter: nbg1-dc3
+          network: kubermatic-e2e
     vsphere-ger:
       location: Hamburg
       country: DE

hack/run-user-cluster-controller-manager.sh
@@ -20,13 +20,14 @@ cd $(dirname $0)/..
 source hack/lib.sh
 
 KUBERMATIC_DEBUG=${KUBERMATIC_DEBUG:-true}
+PPROF_PORT=${PPROF_PORT:-6600}
 
 echodate "Compiling user-cluster-controller-manager..."
 make user-cluster-controller-manager
 
 # Getting everything we need from the api
 # This script assumes you are in your cluster namespace, which you can configure via `kubectl config set-context --current --namespace=<<cluster-namespace>>`
-NAMESPACE="${NAMESPACE:-$(kubectl config view --minify | grep namespace |awk '{ print $2 }')}"
+NAMESPACE="${NAMESPACE:-$(kubectl config view --minify | grep namespace | awk '{ print $2 }')}"
 CLUSTER_NAME="$(echo $NAMESPACE | sed 's/cluster-//')"
 CLUSTER_RAW="$(kubectl get cluster $CLUSTER_NAME -o json)"
 CLUSTER_URL="$(echo $CLUSTER_RAW | jq -r '.address.url')"
@@ -36,22 +37,22 @@ if [ -z "${OWNER_EMAIL:-}" ]; then
   exit 1
 fi
 
-# We can not use the `admin-kubeconfig` secret because the user-cluster-controller-manager is
+# We cannot use the `admin-kubeconfig` secret because the user-cluster-controller-manager is
 # the one creating it in case of openshift. So we just use the internal kubeconfig and replace
-# the apiserver uurl
+# the apiserver url
 KUBECONFIG_USERCLUSTER_CONTROLLER_FILE=$(mktemp)
-ADMIN_KUBECONFIG="$(kubectl get secret internal-admin-kubeconfig -o json \
-  | jq '.data.kubeconfig' -r \
-  | base64 -d \
-  | yaml2json \
-  | jq --arg url "$CLUSTER_URL" '.clusters[0].cluster.server = $url')"
-echo $ADMIN_KUBECONFIG > $KUBECONFIG_USERCLUSTER_CONTROLLER_FILE
+kubectl --namespace "$NAMESPACE" get secret internal-admin-kubeconfig -o json | \
+  jq '.data.kubeconfig' -r | \
+  base64 -d | \
+  yq r --tojson - | \
+  jq --arg url "$CLUSTER_URL" '.clusters[0].cluster.server = $url' \
+  > $KUBECONFIG_USERCLUSTER_CONTROLLER_FILE
 echo "Using kubeconfig $KUBECONFIG_USERCLUSTER_CONTROLLER_FILE"
 
-OPENVPN_SERVER_SERVICE_RAW="$(kubectl get service openvpn-server -o json)"
+OPENVPN_SERVER_SERVICE_RAW="$(kubectl --namespace "$NAMESPACE" get service openvpn-server -o json)"
 
-SEED_SERVICEACCOUNT_TOKEN="$(kubectl get secret -o json \
-  | jq '.items[]|select(.metadata.annotations["kubernetes.io/service-account.name"] == "kubermatic-usercluster-controller-manager")|.data.token' -r \
+SEED_SERVICEACCOUNT_TOKEN="$(kubectl --namespace "$NAMESPACE" get secret -o json \
+  | jq -r '.items[]|select(.metadata.annotations["kubernetes.io/service-account.name"] == "kubermatic-usercluster-controller-manager")|.data.token' \
   | base64 -d)"
 SEED_KUBECONFIG=$(mktemp)
 kubectl config view  --flatten --minify -ojson \
@@ -77,6 +78,7 @@ set -x
   -kubeconfig=${KUBECONFIG_USERCLUSTER_CONTROLLER_FILE} \
   -metrics-listen-address=127.0.0.1:8087 \
   -health-listen-address=127.0.0.1:8088 \
+  -pprof-listen-address=":${PPROF_PORT}" \
   -namespace=${NAMESPACE} \
   -openvpn-server-port=${OPENVPN_SERVER_NODEPORT} \
   -cluster-url=${CLUSTER_URL} \

pkg/api/v1/types.go
@@ -1212,6 +1212,9 @@ type HetznerNodeSpec struct {
 	// server type
 	// required: true
 	Type string `json:"type"`
+	// network name
+	// required: false
+	Network string `json:"network"`
 }
 
 func (spec *HetznerNodeSpec) MarshalJSON() ([]byte, error) {
@@ -1226,9 +1229,11 @@ func (spec *HetznerNodeSpec) MarshalJSON() ([]byte, error) {
 	}
 
 	res := struct {
-		Type string `json:"type"`
+		Network string `json:"network,omitempty"`
+		Type    string `json:"type"`
 	}{
-		Type: spec.Type,
+		Network: spec.Network,
+		Type:    spec.Type,
 	}
 
 	return json.Marshal(&res)

pkg/api/v1/types_test.go
@@ -205,9 +205,10 @@ func TestHetznerNodeSpec_MarshalJSON(t *testing.T) {
 		{
 			"case 2: should marshal when type is provided",
 			&HetznerNodeSpec{
-				Type: "test-type",
+				Type:    "test-type",
+				Network: "test",
 			},
-			"{\"type\":\"test-type\"}",
+			"{\"network\":\"test\",\"type\":\"test-type\"}",
 		},
 	}
 

commit 95d6ef4a5da13f2bb43c253c12ce3c3d72ffa3c6

Fix openstack cloud controller name
This fixes the bug with the OpenStack cloud controller name mismatch
introduced by https://github.com/kubermatic/kubermatic/pull/6619.

pkg/resources/cloudcontroller/openstack.go
@@ -31,7 +31,7 @@ import (
 )
 
 const (
-	osName = "openstack-cloud-controller-manager"
+	OpenstackCCMDeploymentName = "openstack-cloud-controller-manager"
 )
 
 var (
@@ -49,17 +49,17 @@ var (
 
 func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedDeploymentCreatorGetter {
 	return func() (string, reconciling.DeploymentCreator) {
-		return osName, func(dep *appsv1.Deployment) (*appsv1.Deployment, error) {
-			dep.Name = osName
-			dep.Labels = resources.BaseAppLabels(osName, nil)
+		return OpenstackCCMDeploymentName, func(dep *appsv1.Deployment) (*appsv1.Deployment, error) {
+			dep.Name = OpenstackCCMDeploymentName
+			dep.Labels = resources.BaseAppLabels(OpenstackCCMDeploymentName, nil)
 
 			dep.Spec.Replicas = resources.Int32(1)
 
 			dep.Spec.Selector = &metav1.LabelSelector{
-				MatchLabels: resources.BaseAppLabels(osName, nil),
+				MatchLabels: resources.BaseAppLabels(OpenstackCCMDeploymentName, nil),
 			}
 
-			podLabels, err := data.GetPodTemplateLabels(osName, dep.Spec.Template.Spec.Volumes, nil)
+			podLabels, err := data.GetPodTemplateLabels(OpenstackCCMDeploymentName, dep.Spec.Template.Spec.Volumes, nil)
 			if err != nil {
 				return nil, err
 			}
@@ -101,7 +101,7 @@ func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedD
 			dep.Spec.Template.Spec.Containers = []corev1.Container{
 				*openvpnSidecar,
 				{
-					Name:    osName,
+					Name:    ccmContainerName,
 					Image:   data.ImageRegistry(resources.RegistryDocker) + "/k8scloudprovider/openstack-cloud-controller-manager:v" + version,
 					Command: []string{"/bin/openstack-cloud-controller-manager"},
 					Args:    getOSFlags(data),
@@ -114,7 +114,7 @@ func openStackDeploymentCreator(data *resources.TemplateData) reconciling.NamedD
 			}
 
 			defResourceRequirements := map[string]*corev1.ResourceRequirements{
-				osName:              osResourceRequirements.DeepCopy(),
+				ccmContainerName:    osResourceRequirements.DeepCopy(),
 				openvpnSidecar.Name: openvpnSidecar.Resources.DeepCopy(),
 			}
 			err = resources.SetResourceRequirements(dep.Spec.Template.Spec.Containers, defResourceRequirements, nil, dep.Annotations)